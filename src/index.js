42bc534dcaa2e461575d4bc52e59a8e40bad2374
Ljur777 <grygorovychliuda@gmail.com>
2020-10-26 00:07:59 +0200

    feat js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
+        var e001=['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve'] 
+        var e010=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+        var str
+        var r,t
+      //  var e100=["hundred"]
+          
+   // if (number % 100 === 0) {
+   //    return Math.trunk(number/100)+'hundred'+e001[number]
+     if (number <= 12) 
+            return e001[number]
+            else
+      if (number % 10 === 0) 
+      return e010[number/10-2]
+       else {
+         r = e010[number/10-2]
+         t = e001[number[number.length]]
+         return str=(r+' '+t)
+       }
+          }
   
-}
+
